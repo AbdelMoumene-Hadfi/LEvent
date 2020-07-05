c77b442c32d1b57650b6a9b48581e9f038cf88fc
0xHadfi <abdelmouminhadfi15@gmail.com>
2020-07-05 02:38:55 +0100

    Add : get device name from event file .
    Co-Authored-By: AbdelMoumene-Hadfi <AbdelMoumene-Hadfi@users.noreply.github.com>

--- event.c
+++ event.c
@@ -6,19 +6,43 @@
 #include <sys/types.h>
 #include <dirent.h>
 
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <sys/ioctl.h>
+
+#include <linux/input.h>
+
 DIR *fd_d ;
 struct dirent *pDir;
 int main() {
+    int fd_f , count =0;
+    char file_path[50],name[256]="Unkown";
     if((fd_d = opendir("/dev/input/")) == NULL ) {
       perror("unable to open DIR");
       return EXIT_FAILURE;
     }
     while((pDir = readdir(fd_d))!=NULL) {
+      /*get just file*/
       if(pDir->d_type == DT_CHR) {
-        printf("[%s]\n",pDir->d_name);
-      }
+        strcpy(file_path,"/dev/input/");
+        strcat(file_path,pDir->d_name);
+        if((fd_f = open(file_path,O_RDONLY))<0) {
+          perror("enable to open file");
+          continue ;
+        }
+        if(ioctl(fd_f, EVIOCGNAME(sizeof(name)), name) < 0) {
+          perror("enable to control device");
+          close(fd_f);
+          continue;
+        }
+        printf("[%d] %s : %s\n",count,file_path,name);
+        count++;
+        close(fd_f);
 
+      }
     }
+
     closedir(fd_d);
     return EXIT_SUCCESS;
 }
